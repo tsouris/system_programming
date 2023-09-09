4f47cf2a18f9c70127655cdc879147a954eb8294
tsouris <nikolastsouris16@gmail.com>
2023-09-09 13:16:24 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+
+namespace system_programming
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<Process> processList = new List<Process>(Process.GetProcesses());
+            processListView.Items.Clear();
+            foreach (var process in processList)
+            {
+                processListView.Items.Add(new { Name = process.ProcessName, Id = process.Id });
+            }
+        }
+    }
+}
